95840437aa4b4013ee9ec8c930fde4044ffe1af3
Karol Wawrzecki <kwawrzecki@gmail.com>
2016-11-11 17:28:11 +0100

    initial project setup

---
+++ src/test/simple.spec-compiled.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var chai = require('chai');
+chai.should();
+
+describe('Simple setup', function () {
+
+    it('should success', function () {
+        var a = true;
+        a.should.equal(true);
+    });
+});
+
+//# sourceMappingURL=simple.spec-compiled.js.map
---
+++ src/test/simple.spec.js
@@ -0,0 +1,12 @@
+'use strict';
+let chai = require('chai');
+chai.should();
+
+describe('Simple setup', () => {
+
+    it('should success', () => {
+        let a = true;
+        a.should.equal(true);
+    });
+
+});
